84a7f3309f8ac40e38dd06a3b69f55fec7159c4e
Aishwarya Sardae <109073392+aishwaryasardae@users.noreply.github.com>
2024-06-01 23:53:13 -0400

    Add files via upload

---
+++ Database/db.sql
@@ -0,0 +1,36 @@
+/*
+SQLyog Community Edition- MySQL GUI v6.07
+Host - 5.5.30 : Database - covid
+*********************************************************************
+Server version : 5.5.30
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+create database if not exists `covid`;
+
+USE `covid`;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+/*Table structure for table `user_reg` */
+
+DROP TABLE IF EXISTS `user_reg`;
+
+CREATE TABLE `user_reg` (
+  `name` varchar(100) DEFAULT NULL,
+  `user_name` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `phno` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `user_reg` */
+
+insert  into `user_reg`(`name`,`user_name`,`password`,`email`,`phno`) values ('cloudtechnologies','ct123','ct123','ct@gmail.com','8121953811'),('CLOUDTECHNOLOGIES','ct1234','ct1234','ct@gmail.com','8121953811');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
